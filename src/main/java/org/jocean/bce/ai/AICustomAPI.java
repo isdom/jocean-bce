667837a96e53b521969c5f09decdb8ad6c46c4b4
ming.ma <isdom@qq.com>
2019-08-13 10:38:03 +0800

    实现 AICustomAPI.detectObject: 基于自定义训练出的物体检测模型，实现定制图像识别。参见: http://ai.baidu.com/docs#/EasyDL_VIS_API/17ff0cc2

---
+++ src/main/java/org/jocean/bce/ai/AICustomAPI.java
@@ -0,0 +1,75 @@
+package org.jocean.bce.ai;
+
+import org.jocean.http.RpcRunner;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+import rx.Observable.Transformer;
+
+// http://ai.baidu.com/docs#/Auth : 鉴权认证机制
+
+public interface AICustomAPI {
+
+    public interface ObjectLocation {
+        @JSONField(name = "left")
+        public int getLeft();
+
+        @JSONField(name = "left")
+        public void setLeft(final int left);
+
+        @JSONField(name = "top")
+        public int getTop();
+
+        @JSONField(name = "top")
+        public void setTop(final int top);
+
+        @JSONField(name = "width")
+        public int getWidth();
+
+        @JSONField(name = "width")
+        public void setWidth(final int width);
+
+        @JSONField(name = "height")
+        public int getHeight();
+
+        @JSONField(name = "height")
+        public void setHeight(final int height);
+    }
+
+    public interface DetectionResult {
+        @JSONField(name = "name")
+        public String getName();
+
+        @JSONField(name = "name")
+        public void setName(final String name);
+
+        @JSONField(name = "score")
+        public float getScore();
+
+        @JSONField(name = "score")
+        public void setScore(final float score);
+
+        @JSONField(name = "location")
+        public ObjectLocation getLocation();
+
+        @JSONField(name = "location")
+        public void setLocation(final ObjectLocation location);
+    }
+
+    public interface ObjectDetectionResponse {
+        @JSONField(name = "log_id")
+        public int getLogId();
+
+        @JSONField(name = "log_id")
+        public void setLogId(final int logid);
+
+        @JSONField(name = "results")
+        public DetectionResult[] getResults();
+
+        @JSONField(name = "results")
+        public void setResults(final DetectionResult[] results);
+    }
+
+    public Transformer<RpcRunner, ObjectDetectionResponse> detectObject(
+            final String appname, final String apipath, final String imageAsBase64);
+}
---
+++ src/main/java/org/jocean/bce/ai/internal/DefaultAICustomAPI.java
@@ -0,0 +1,56 @@
+package org.jocean.bce.ai.internal;
+
+import javax.inject.Inject;
+
+import org.jocean.bce.ai.AICustomAPI;
+import org.jocean.bce.oauth.OAuthAPI;
+import org.jocean.http.ContentUtil;
+import org.jocean.http.RpcRunner;
+import org.jocean.idiom.BeanFinder;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+import io.netty.handler.codec.http.HttpMethod;
+import rx.Observable.Transformer;
+
+public class DefaultAICustomAPI implements AICustomAPI {
+
+    public class ImageData {
+        @JSONField(name = "image")
+        public String getImage() {
+            return this._image;
+        }
+
+        @JSONField(name = "image")
+        public void setImage(final String imageAsBase64) {
+            this._image = imageAsBase64;
+        }
+
+        String _image;
+    }
+
+    /*
+    http://ai.baidu.com/docs#/EasyDL_VIS_API/17ff0cc2
+    */
+
+    @Override
+    public Transformer<RpcRunner, ObjectDetectionResponse> detectObject(
+            final String appname, final String apipath, final String imageAsBase64) {
+        final ImageData data = new ImageData();
+
+        data.setImage(imageAsBase64);
+
+        return runners -> _finder.find(appname, OAuthAPI.class).flatMap(oauth -> runners.compose(oauth.getAccessToken()))
+                .flatMap(oauthresp -> runners.flatMap(runner -> runner.name("bce.ai.objectDetection")
+                        .execute(interact -> interact
+                                .method(HttpMethod.POST)
+                                .uri("https://aip.baidubce.com")
+                                .path("/rpc/2.0/ai_custom/v1/detection/" + apipath)
+                                .paramAsQuery("access_token", oauthresp.getAccessToken())
+                                .body(data, ContentUtil.TOJSON)
+                                .responseAs(ContentUtil.ASJSON, ObjectDetectionResponse.class))));
+    }
+
+    @Inject
+    BeanFinder _finder;
+}
